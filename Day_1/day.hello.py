0d56bb5666dddcd5c7e97dfea52b50d21754b4ea
Rohit Kumar <rohitkumarmandal34@gmail.com>
2025-08-22 11:14:36 +0530

    first commit

---
+++ Day_1/day.hello.py
@@ -0,0 +1,10 @@
+print("Hello, World! I'm starting my coding journey 🚀")
+
+# Storing some information about me
+name = "Rohit"
+experience_years = 3.9
+goal = "Learn Python + Data Engineering"
+
+print("Name:", name)
+print("Experience:", experience_years, "years")
+print("Goal:", goal)
